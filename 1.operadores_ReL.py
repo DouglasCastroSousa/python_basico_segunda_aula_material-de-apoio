7fa14a5896e37b48084e79cc7fae799d298cae80
Douglas <douglascastro1086@gmail.com>
2025-11-08 12:56:04 -0300

    Aula 2 de Python

---
+++ 1.operadores_ReL.py
@@ -0,0 +1,24 @@
+### Operadores Relacionais
+### TRUE / FALSE ###
+
+x = 5 > 7 #False
+
+y = 6 != 6 #False
+
+z = y == x #True
+
+print(z)
+
+### Operadores lógicos
+
+a = True
+print(not a)  #False
+
+b = False
+print(not b)  #True
+
+
+print(not 0)      # True (porque 0 é considerado falso)
+print(not 10)     # False (porque qualquer número ≠ 0 é verdadeiro)
+print(not "")     # True (string vazia é falsa)
+print(not "Oi")   # False (string não vazia é verdadeira)
